c66bc32975613526275ad5654af682f5f8aca056
Anna Zadiraka <annazadiraka@gmail.com>
2025-07-31 12:47:45 +0300

    creating a project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+   <p class="p-2 mb-4 inline-block bg-indigo-100">Для указания нескольких меток для одной пар логин/пароль используйте разделитель ;</p>
+</template>
+
+<style scoped>
+
+</style>
